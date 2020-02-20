c6f330930b69f1218b6342f49c56729d7a08c70a
Elián Tapia Santa-Gadea <Elianetapia@users.noreply.github.com>
2020-02-20 09:43:24 -0500

    segunda actualización

--- src/validator.js
+++ src/validator.js
@@ -1,8 +1,42 @@
 function stringToArray(){
-  const numberByNumber = creditCardNumber.split("");
-  document.getElementById('creditCardNumber').innerHTML= creditCardNumber
-  (); 
- }
+  const numberByNumber = creditCardNumber.value.split('').reverse().join();
+return numberByNumber;
+}
+
+function arrayReverse(){
+
+  const numberReverse = numberByNumber.reverse();
+  return numberReverse;
+  console.log(numberReverse);
+} 
+
+function stringToNumber(){
+  const strToNumber = parseInt(numberReverse);
+  return strToNumber;
+  
+};
+/*function separateNumbers(){
+  for( let i=0; < number.length, i++){
+
+  }
+}
+//codigo trabajado con Michelle
+/*const myStr = '1234567789872';
+
+
+function stringToNumber(){
+  const strToNumber = parseInt(myStr);
+  console.log(strToNumber);
+  return strToNumber;
+};
+
+const number = stringToNumber(myStr);
+
+function separateNumbers(){
+for( let i = 0; i < number.length; i++){
+  
+}
+}*/
 
 
 
