93ed85bac0cbb852fbc8ea8f6c3ef4b49a84681b
Elián Tapia Santa-Gadea <Elianetapia@users.noreply.github.com>
2020-02-23 16:36:06 -0500

    actualizando validator

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import validator from './validator.js';
 
 const creditCardNumber = document.getElementById('creditCardNumber');
 const sendButton = document.getElementById('sendButton');
+const respuestaUsuario = document.getElementById('respuesta-usuario');
 
+const informacionUsuario = (event) => {
+   event.preventDefault();
+   respuestaUsuario.innerText = `Tu tarjeta ${validator.maskify(creditCardNumber.value)} es ${validator.isValid(creditCardNumber.value)}`
+};
 
-sendButton.addEventListener('click',() => {
-    console.log (creditCardNumber.value);
-});
-
+sendButton.addEventListener('click', informacionUsuario);
--- src/validator.js
+++ src/validator.js
@@ -1,22 +1,15 @@
-//input
-const myStr = '12745129410';
-
-01492154721
-0 4 2 5 7 1
-0 8 4 10 14 2
-
-//funcion que va a trabajar sobre ese input
-function stringToArray(){
-  const myArray = myStr.split('');
-  return myArray;
+function stringToArray(creditCardNumber){
+  let creditCardArray = [];
+  for (let i = 0; i < creditCardNumber.length; i++) {
+      const charToNumber = parseInt(creditCardNumber.charAt(i))
+      creditCardArray.push(charToNumber);
+  }
+  return creditCardArray;
 }
 
-//invocar la funcion y pasarle como argumento el input
-//guardar el resultado de esto en una nueva variable
-//que será mi siguiente input para mi siguiente función
-const newArray = stringToArray(myStr);
+const newArray = stringToArray(creditCardNumber);
 
-function reverseArray(){
+function reverseArray(newArray){
   const reversedArray = [];
   for(let i=newArray.length -1; i>=0 ; i--){
     let number = parseInt(newArray[i]);
@@ -28,7 +21,7 @@ return reversedArray;
 const arrayReversed = reverseArray(newArray);
 
 function pares(){
-  sonPares = [];
+  const sonPares = [];
   for (let i=0; i<arrayReversed.length; i++) {
        if (i/2 === Math.round(i/2)) {
           sonPares.push(arrayReversed[i]);
@@ -38,16 +31,25 @@ function pares(){
 }
 
 const evenNumbers = pares(arrayReversed);
+
+function impares(){
+  const numerosImpares = [];
+  for (let i=0; i<arrayReversed.length; i++) {
+       if (i%2 > 0) {
+          numerosImpares.push(arrayReversed[i]);
+       }
+    }
+  return numerosImpares;
+}
+
+const oddNumbers = impares(arrayReversed);
  
-function dobleNumbers(){
+function doubleNumbers(){
   let sumatoria = [];
   for (let i=0; i<evenNumbers.length; i++){
     let multiplied = evenNumbers[i] * 2
     if(multiplied >=10){
-      /*for(let i=0;i<multiplied.length;i++){
-        let suma = suma + parseInt(valor[i]);
-        sumatoria.push(suma);
-        }*/
+      sumatoria.push(multiplied%9);
       }else{
       sumatoria.push(multiplied);
       }
@@ -55,81 +57,50 @@ function dobleNumbers(){
   return sumatoria;
 }
 
-dobleNumbers(evenNumbers);
+const finalEvenNumbers = doubleNumbers(evenNumbers);
 
-//atrapando numeros en posiciones pares
-
-/*
-function separateNumbers(){
-  for( let i=0; < number.length, i++){
-
-  }
+function joinNumbers(oddNumbers, finalEvenNumbers){
+  const numeroTotal = oddNumbers.concat(finalEvenNumbers);
+  return numeroTotal
 }
-//CODIGO TRABAJADO CON MICHELLE
-/*const myStr = '1234567789872';
 
+const totalNumbers = joinNumbers(oddNumbers, finalEvenNumbers);
 
-function stringToNumber(){
-  const strToNumber = parseInt(myStr);
-  console.log(strToNumber);
-  return strToNumber;
+function addTotalNumbers(){
+let suma = 0;
+for (let i=0; i<totalNumbers.length; i++){
+        suma = suma + totalNumbers[i];     
+        console.log(suma);
+      }
+return suma;
 };
 
-const number = stringToNumber(myStr);
+const allNumbers = addTotalNumbers(totalNumbers);
 
-function separateNumbers(){
-for( let i = 0; i < number.length; i++){
-  
+function hallandoResiduos(){
+  if (allNumbers % 10 === 0) {
+    return true;
+} else {
+    return false;
 }
 }
-
-CODIGO DE JOYLOVE
-//Call the function
-isValidCard (inputCodeCard)
-
-//Create the function to valid card
-function isValidCard (codeCard){
-
-    var arrReverse = []; //Create the array.reverse
-    for (var i = 0; i < codeCard.length; i++){
-          arrReverse.unshift(parseInt(codeCard[i]));
-    }
-
-    //Getting numbers in even position
-    for (var j = 1; j <arrReverse.length; j = j+2){
-
-      if (arrReverse[j]*2 >= 10){
-        var numberByTwo = arrReverse[j]*2;
-        //If the value of the multiplication is two digits: add digits and then replace in arrReverse
-        arrReverse[j] = parseInt(numberByTwo.toString(10)[0]) + parseInt(numberByTwo.toString(10)[1])
-        }else{
-          //If the value of the multiplication is one digit, it is replaced in arrReverse
-          arrReverse[j] = arrReverse[j]*2
-        }
-
-
-      //Adding card digits
-      var sum = 0
-      for (var k = 0; k < arrReverse.length; k++){
-        sum = sum + arrReverse[k]
-      }
-    }
-
-    //Validate card
-    if (sum % 10 === 0) {
-      return document.write ("Tu número de tarjeta es VÁLIDA")
-      }else{
-        return document.write ("Tu número de tarjeta es INVÁLIDA")
-      }
-   }
-*/
-
-
-
-
 
 const validator = {
-  // ...
+  isValid: (creditCardNumber) => {
+    const newArray = stringToArray(creditCardNumber);
+    const arrayReversed = reverseArray(newArray);
+    const evenNumbers = pares(arrayReversed);
+    const oddNumbers = impares(arrayReversed);
+    const finalEvenNumbers = doubleNumbers(evenNumbers);
+    const totalNumbers = joinNumbers(oddNumbers, finalEvenNumbers);
+    const allNumbers = addTotalNumbers(totalNumbers);
+    const finalValue = hallandoResiduos(allNumbers);
+    return finalValue;
+  },
+
+  maskify: (creditCardNumber) => {
+    return creditCardNumber.replace(/\d(?=\d{4})/g, "#");
+  }
 };
 
 export default validator;
